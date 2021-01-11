3bde874e79a194a0ba5a8f2314ec0e83debe2fb1
sondoskareem <sondosabd94@gmail.com>
2021-01-11 21:08:25 +0300

    final

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(path.join(__dirname + "/dist")));
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
