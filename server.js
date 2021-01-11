04d1f793a090a8523ab6837e0eb0e908e1939c16
sondoskareem <sondosabd94@gmail.com>
2021-01-11 22:40:40 +0300

    make it better

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(path.join(__dirname + "/dist")));
 
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
-  
+// if (process.env.NODE_ENV === "production"){
+//     app.use(express.static("build"));
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+//     });
+//   }
+
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
