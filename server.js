f46814dbafc27d9ceff7af310dd8cab5453bda77
sondoskareem <sondosabd94@gmail.com>
2021-01-11 22:38:42 +0300

    make it better

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(path.join(__dirname + "/dist")));
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
+  
 var port = process.env.PORT || 8080;
 app.listen(port);
 console.log('server started '+ port);
